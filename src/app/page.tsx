56e807d4d099b459cff3fc07bb2a4d92dd4b0bba
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-08-31 00:00:20 +0300

    loading screen && some pages update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,7 @@ import Footer from "@/components/footer"
 import Hero from "@/components/hero"
 import Projects from "@/components/projects"
 import SlideImages from "@/components/slide-images"
-import { Metadata } from "next"
 import PrealoadWrapper from "@/components/preaload-wrapper"
-
-export const metadata: Metadata = {
-  metadataBase: new URL("https://nextaddis.com"),
-  title:
-    "Digital Agency | Web Development, UI/UX Design, Mobile App Development, Software Development",
-  description:
-    "We are a digital agency that provides a wide range of services, including web development, UI/UX design, mobile app development, and software development. We help businesses of all sizes achieve their digital goals by creating innovative and user-friendly solutions.",
-}
 
 export default function Home() {
   return (
