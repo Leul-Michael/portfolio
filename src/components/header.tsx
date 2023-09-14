cdde48dd1776ccb9ea7c3d69adf3d81da1eabd80
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-09-15 00:15:28 +0300

    overflow curve added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const setShowMenu = useSetShowMenu()
 
   return (
-    <header className="fixed z-[99] top-0 left-0 w-full h-[70px] md:px-16 px-5 flex flex-col">
+    <header className="fixed z-[99] top-0 left-0 w-screen h-[70px] md:px-16 px-5 flex flex-col">
       <div className="flex justify-between items-center gap-4 flex-wrap py-2">
         <h1 className="lg:text-[2rem] md:text-[1.65rem] text-[1.25rem] font-bold">
           Leul Michael ©
