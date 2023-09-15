c63968c9705f79ba25d2b7dd075235adb8658eaa
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-09-16 00:54:12 +0300

    about page added

--- src/components/preaload-wrapper.tsx
+++ src/components/preaload-wrapper.tsx
@@ -5,12 +5,15 @@ import PreaLoader from "./preloader"
 import imagesLoaded from "imagesloaded"
 import useScroll from "@/context/ScrollProvider"
 import AnimatePresenceWrapper from "./AnimatePresenceWrapper"
+import { usePathname } from "next/navigation"
 
 const PrealoadWrapper = () => {
+  const pathname = usePathname()
   const { lenis } = useScroll()
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
+    if (pathname !== "/") return
     imagesLoaded(document.querySelector("#hero")!, function () {
       setTimeout(() => {
         setIsLoading(false)
@@ -18,15 +21,20 @@ const PrealoadWrapper = () => {
         window.scrollTo(0, 0)
       }, 2000)
     })
-  }, [])
+  }, [pathname])
 
   useEffect(() => {
+    if (pathname !== "/") return
     if (isLoading) {
       lenis?.stop()
     } else {
       lenis?.start()
     }
-  }, [lenis, isLoading])
+  }, [lenis, isLoading, pathname])
+
+  if (pathname !== "/") {
+    return null
+  }
 
   return (
     <AnimatePresenceWrapper>
