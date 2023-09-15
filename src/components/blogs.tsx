b42b769f44f7d236e0ae97258036fd6f31cf350d
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-09-15 14:10:00 +0300

    blogs project in sync

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -4,11 +4,13 @@ import { motion, useScroll, useTransform } from "framer-motion"
 import gsap from "gsap"
 import Image from "next/image"
 import { useEffect, useRef, useState } from "react"
+import { cn } from "@/lib/utils"
 
 import Wrapper from "./wrapper"
 import Button from "./button"
 import BlogExcerpt from "./blog-excerpt"
 import blogs from "@/lib/blog.json"
+import { useProjectBg } from "@/lib/zustand"
 
 const scaleAnimation = {
   initial: { scale: 0, x: "-50%", y: "-50%" },
@@ -27,6 +29,8 @@ const scaleAnimation = {
 }
 
 const Blogs = () => {
+  const projectBg = useProjectBg()
+
   const [modal, setModal] = useState({ active: false, index: 0 })
   const { active, index } = modal
   const modalContainer = useRef(null)
@@ -71,61 +75,68 @@ const Blogs = () => {
   return (
     <section
       ref={container}
-      className="relative flex flex-col py-20 w-full h-full z-[2] bg-background"
+      className={cn("relative flex flex-col w-full h-full z-[2] bg-background")}
     >
-      <Wrapper
-        onMouseMove={(e) => {
-          moveItems(e.clientX, e.clientY)
-        }}
-        className="flex flex-col gap-16"
+      <div
+        className={cn(
+          "hero-wrapper flex flex-col w-full h-full py-20",
+          projectBg
+        )}
       >
-        <h1 className="text-2xl font-bold py-2">Blogs</h1>
-        <div className="flex flex-col">
-          {blogs.blogs.map((blog) => (
-            <BlogExcerpt
-              key={blog.slug}
-              blog={blog}
-              manageModal={manageModal}
-            />
-          ))}
-        </div>
-        <Button
-          name="More Blogs"
-          className="px-6 py-2 self-start text-xl capitalize font-semibold rounded-full text-primary-muted"
-        />
-      </Wrapper>
-      <motion.div
-        ref={modalContainer}
-        variants={scaleAnimation}
-        initial="initial"
-        animate={active ? "enter" : "closed"}
-        className="fixed w-[250px] h-[200px] rounded-lg top-[50%] left-[50%] z-[3] pointer-events-none overflow-hidden"
-      >
-        <div
-          style={{ top: index * -100 + "%" }}
-          className="relative w-full h-full blog-img-slider"
+        <Wrapper
+          onMouseMove={(e) => {
+            moveItems(e.clientX, e.clientY)
+          }}
+          className="flex flex-col gap-16"
         >
-          {blogs.blogs.map((blog, index) => {
-            const { imgPath, color } = blog
-            return (
-              <div
-                className="w-full h-full flex justify-center items-center"
-                style={{ backgroundColor: color }}
-                key={`modal_${index}`}
-              >
-                <Image
-                  src={imgPath}
-                  width={250}
-                  height={0}
-                  alt="image"
-                  className="object-cover w-auto h-full"
-                />
-              </div>
-            )
-          })}
-        </div>
-      </motion.div>
-      <motion.div style={{ height }} className="blogs-overlay"></motion.div>
+          <h1 className="text-2xl font-bold py-2">Blogs</h1>
+          <div className="flex flex-col">
+            {blogs.blogs.map((blog) => (
+              <BlogExcerpt
+                key={blog.slug}
+                blog={blog}
+                manageModal={manageModal}
+              />
+            ))}
+          </div>
+          <Button
+            name="More Blogs"
+            className="px-6 py-2 self-start text-xl capitalize font-semibold rounded-full text-primary-muted"
+          />
+        </Wrapper>
+        <motion.div
+          ref={modalContainer}
+          variants={scaleAnimation}
+          initial="initial"
+          animate={active ? "enter" : "closed"}
+          className="fixed w-[250px] h-[200px] rounded-lg top-[50%] left-[50%] z-[3] pointer-events-none overflow-hidden"
+        >
+          <div
+            style={{ top: index * -100 + "%" }}
+            className="relative w-full h-full blog-img-slider"
+          >
+            {blogs.blogs.map((blog, index) => {
+              const { imgPath, color } = blog
+              return (
+                <div
+                  className="w-full h-full flex justify-center items-center"
+                  style={{ backgroundColor: color }}
+                  key={`modal_${index}`}
+                >
+                  <Image
+                    src={imgPath}
+                    width={250}
+                    height={0}
+                    alt="image"
+                    className="object-cover w-auto h-full"
+                  />
+                </div>
+              )
+            })}
+          </div>
+        </motion.div>
+        <motion.div style={{ height }} className="blogs-overlay"></motion.div>
+      </div>
     </section>
   )
 }
--- src/components/hero-wrapper.tsx
+++ src/components/hero-wrapper.tsx
@@ -2,21 +2,16 @@
 
 import { cn } from "@/lib/utils"
 import { useProjectBg } from "@/lib/zustand"
-import { ReactNode, useEffect, useState } from "react"
+import { ReactNode } from "react"
 
 const HeroWrapper = ({ children }: { children: ReactNode }) => {
   const projectBg = useProjectBg()
-  const [bg, setBg] = useState(projectBg)
-
-  useEffect(() => {
-    setBg(projectBg)
-  }, [projectBg])
 
   return (
     <section
       className={cn(
         "hero-wrapper flex flex-col w-full h-full bg-background",
-        bg
+        projectBg
       )}
     >
       {children}
